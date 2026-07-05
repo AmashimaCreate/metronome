3ec3aec949c0b7386cfc326d20024c0b3dc605d5
KGguitar <tamaken.lespo@gmail.com>
2026-07-06 05:13:36 +0900

    Add PWA support

---
+++ sw.js
@@ -0,0 +1,64 @@
+const CACHE_NAME = "metronome-pwa-v1";
+const LOCAL_ASSETS = [
+  "./",
+  "./index.html",
+  "./favicon.svg",
+  "./pwa-192.png",
+  "./pwa-512.png",
+  "./og-image.png",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(LOCAL_ASSETS))
+      .catch(() => {})
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME)
+            .then(cache => cache.put("./index.html", copy))
+            .catch(() => {});
+          return response;
+        })
+        .catch(() => caches.match("./index.html"))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+
+      return fetch(event.request).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME)
+          .then(cache => cache.put(event.request, copy))
+          .catch(() => {});
+        return response;
+      });
+    })
+  );
+});
